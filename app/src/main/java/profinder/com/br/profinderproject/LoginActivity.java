177763d13c2ca3e7b9fce1beacbf47b5536263f9
Leonardo Leitao <covadevs@gmail.com>
2018-03-03 21:40:55 -0300

    Finalizacao layout tela login e inicio da implementacao da mesma. tela de cadastro ainda nao implementada.. criada apenas para teste por enquanto

--- app/src/main/java/profinder/com/br/profinderproject/LoginActivity.java
+++ app/src/main/java/profinder/com/br/profinderproject/LoginActivity.java
@@ -1,13 +1,50 @@
 package profinder.com.br.profinderproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView mUsuario;
+    private TextView mSenha;
+    private TextView mSignUp; //cadastre-se
+    private Button mEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initComponentes();
+        colocarUnderlineEmTexto(this.mSignUp);
+        actionListeners();
+    }
+
+    private void colocarUnderlineEmTexto(TextView t) {
+        SpannableString spannableString = new SpannableString(t.getText().toString());
+        spannableString.setSpan(new UnderlineSpan(), 0, spannableString.length(), 0);
+        t.setText(spannableString);
+    }
+
+    private void initComponentes() {
+        this.mUsuario = (TextView) findViewById(R.id.username_login);
+        this.mSenha = (TextView) findViewById(R.id.password_login);
+        this.mSignUp = (TextView) findViewById(R.id.signup);
+        this.mEntrar = (Button) findViewById(R.id.entrar);
+    }
+
+    private void actionListeners() {
+        this.mSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
